ce15b71920cc39b202cde0cb81aa64300277c925
hyago0897.com <02hyago@gmail.com>
2021-09-07 18:34:20 -0300

    Mudança de nome do pack back-end para back_end

---
+++ back_end/bancoDados/locadora.sql
@@ -0,0 +1,54 @@
+CREATE TABLE IF NOT EXISTS cliente (
+	id_cliente INTEGER PRIMARY KEY AUTOINCREMENT,
+	nome TEXT NOT NULL,
+	cpf TEXT(11) NOT NULL,
+	endereco TEXT NOT NULL,
+	fone TEXT NOT NULL,
+	`e-mail` TEXT,
+	login TEXT NOT NULL,
+	senha TEXT NOT NULL,
+	data_nasci TEXT NOT NULL,
+	`n_CNH` TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS manutencao (
+	id_manutencao INTEGER PRIMARY KEY AUTOINCREMENT,
+	modelo TEXT NOT NULL,
+	custo_mm REAL NOT NULL,
+	descricao TEXT
+);
+
+CREATE TABLE IF NOT EXISTS veiculo (
+	id_veiculo INTEGER PRIMARY KEY AUTOINCREMENT,
+	placa TEXT(7) NOT NULL,
+	marca TEXT NOT NULL,
+	modelo TEXT NOT NULL,
+	cor TEXT,
+	descricao TEXT,
+	ano TEXT,
+	status_veiculo TEXT NOT NULL,
+	id_manutencao INTEGER NOT NULL,
+	FOREIGN key (id_manutencao) REFERENCES manutencao(id_manutencao)
+);
+
+CREATE TABLE IF NOT EXISTS contrato (
+	id_contrato INTEGER PRIMARY KEY AUTOINCREMENT,
+	id_veiculo INTEGER NOT NULL,
+	id_cliente INTEGER NOT NULL,
+	data_inicio TEXT NOT NULL,
+	data_termino TEXT NOT NULL,
+	preco_diaria REAL NOT NULL,
+	seguro TEXT NOT NULL,
+	n_diarias INTEGER NOT NULL,
+	status_contrato TEXT NOT NULL,
+	politica_combustivel TEXT NOT NULL,
+	forma_pagamento TEXT NOT NULL,
+	FOREIGN key (id_veiculo) REFERENCES veiculo(id_veiculo),
+	FOREIGN key (id_cliente) REFERENCES cliente(id_cliente)
+);
+
+CREATE TABLE IF NOT EXISTS admin (
+	login TEXT PRIMARY KEY,
+	senha TEXT NOT NULL,
+	nome TEXT NOT NULL
+);
--- interfaces/login.py
+++ interfaces/login.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 import tkinter.ttk as ttk
+from ..back_end import bancoDados
 
 
 class TelaLogin(tk.Frame):
