99bf7de3365083ac340daabd608d75d44030e9c9
gilsonfish <gilsonpeixotofilho@gmail.com>
2021-09-08 14:07:41 -0300

    Tela login funcionando

--- interfaces/login.py
+++ interfaces/login.py
@@ -1,5 +1,7 @@
+import sqlite3
 import tkinter as tk
 import tkinter.ttk as ttk
+from tkinter import messagebox
 
 
 class TelaLogin(tk.Frame):
@@ -19,8 +21,8 @@ class TelaLogin(tk.Frame):
         self.container2.pack(fill='both', expand=1, padx=3, pady=3)
 
         tk.Label(self.container2, text="Login:").grid(column=0, row=0)
-        self.nome = tk.Entry(self.container2)
-        self.nome.grid(column=1, row=0)
+        self.login = tk.Entry(self.container2)
+        self.login.grid(column=1, row=0)
 
         tk.Label(self.container2, text="Senha:").grid(column=0, row=1)
         self.senha = tk.Entry(self.container2)
@@ -30,9 +32,10 @@ class TelaLogin(tk.Frame):
 
         tk.Checkbutton(self.container2,
                        text="Administrador",
-                       variable=self.checkVar).grid(column=0,
-                                                    row=2,
-                                                    columnspan=2)
+                       variable=self.checkVar,
+                       command=self.alterar_login).grid(column=0,
+                                                        row=2,
+                                                        columnspan=2)
 
         self.btnLimpar = tk.Button(self.container1,
                                    text="LIMPAR",
@@ -40,24 +43,69 @@ class TelaLogin(tk.Frame):
         self.btnLimpar.pack(side='left', fill='x', expand=1, padx=3)
         self.btnLogin = tk.Button(self.container1,
                                   text="LOGIN",
-                                  command=self.verificar_dados)
+                                  command=self.logar_cliente)
         self.btnLogin.pack(side='left', fill='x', expand=1, padx=3)
 
-        self.nome.focus()
+        self.login.focus()
+
+    def get_dados(self):
+        return self.login.get().strip(), self.senha.get().strip()
+
+    def alterar_login(self):
+        if self.checkVar.get():
+            self.btnLogin.configure(command=self.logar_admin)
+        else:
+            self.btnLogin.configure(command=self.logar_cliente)
 
     def limpar_campos(self):
-        self.nome.delete(0, tk.END)
+        self.login.delete(0, tk.END)
         self.senha.delete(0, tk.END)
-        self.nome.focus()
+        self.login.focus()
+
+    def verifica_campos(self):
+        login, senha = self.get_dados()
+        if login == '' or senha == '':
+            messagebox.showinfo(
+                "Aviso", "Preencha os campos login e senha para logar!")
+            return False
+        else:
+            return True
 
     def logar_cliente(self):
-        pass
+        if self.verifica_campos():
+            login, senha = self.get_dados()
+            res = self.bd.cursor().execute(
+                "SELECT login, senha FROM CLIENTES").fetchall()
+            usuarios = [x[0] for x in res]
+            senhas = [x[1] for x in res]
+            if login not in usuarios:
+                messagebox.showinfo("Aviso",
+                                    f"O Login '{login}' não foi encontrado!")
+            elif senhas[usuarios.index(login)] != senha:
+                messagebox.showinfo("Aviso", "Senha incorreta!")
+            else:
+                return login
+        return None
 
     def logar_admin(self):
-        pass
+        if self.verifica_campos():
+            login, senha = self.get_dados()
+            res = self.bd.cursor().execute(
+                "SELECT login, senha FROM ADMIN").fetchall()
+            admins = [x[0] for x in res]
+            senhas = [x[1] for x in res]
+            if login not in admins:
+                messagebox.showinfo("Aviso",
+                                    f"O Login '{login}' não foi encontrado!")
+            elif senhas[admins.index(login)] != senha:
+                messagebox.showinfo("Aviso", "Senha incorreta!")
+            else:
+                return login
+        return None
 
 
 if __name__ == "__main__":
+    bd = sqlite3.connect("teste.db")
     tela = tk.Tk()
-    TelaLogin(tela, '')
+    TelaLogin(tela, bd)
     tela.mainloop()
