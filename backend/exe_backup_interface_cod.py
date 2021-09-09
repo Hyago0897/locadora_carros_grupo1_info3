afc0ff576251a1c7a7a89e8b6fd4d98353011dc1
hyago0897.com <02hyago@gmail.com>
2021-09-09 13:17:00 -0300

    coreções

--- backend/exe_backup_interface_cod.py
+++ backend/exe_backup_interface_cod.py
@@ -5,9 +5,3 @@ import tkinter as tk
 class ExeTarefas(threading.Thread):
     def __init__(self, tarefa):
         threading.Thread.__init__(self, target=tarefa)
-        self.tarefa = tarefa
-
-    @property
-    def tarefa(self): return self._tarefa
-    @tarefa.setter
-    def tarefa(self, value): self._tarefa = value
