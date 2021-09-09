12507126e6bc2f101707ac7a8fd870e8cc71bb58
hyago0897.com <02hyago@gmail.com>
2021-09-08 22:30:15 -0300

    Remoção comentários .\backend__init__.py

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,10 +1,2 @@
 from .conexao import BancoDeDados
 from .backup import Backup
-
-#login_cliente = "SELECT nome  FROM cliente WHERE login = ? and senha = ?"
-#login_admn = "SELECT nome  FROM admin WHERE login = ? and senha = ?"
-
-#def login(usuario):
-#    if usuario == 'adm':
-#        with BancoDeDados() as banco:
-#            pass
